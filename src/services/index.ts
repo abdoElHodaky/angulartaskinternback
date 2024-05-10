6c07f1f1eb25777ce609b217a38d6452219d38e7
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-10 10:09:47 +0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,32 +1,7 @@
-import "reflect-metadata";
-
-import {AuthService} from "./auth";
-import {AttachmentService} from "./attachments";
-import {UserService} from "./users";
-import {AuthorService} from "./authors";
-import {ArticleService} from "./articles";
-import {UserTicketService} from "./users.tickets";
-import {PaymentService } from "./payments";
-import { PayTabService } from "./paytabs"
-const services:any={
-  "Auth":AuthService,
-  "Author":AuthorService,
-  "User":UserService,
-  "Article":ArticleService,
-  "Attachment":AttachmentService,
-  "Ticket":UserTicketService,
-  "Payment":PaymentService,
-  "PayTabGate":PayTabService
-}
-
-export function service(service:string) {
-      return function (target: any, propertyKey?: string  ) {
-      Reflect.defineMetadata("service", service, target);
-      console.log(target)
-      let s=new services[service]()
-      if (target==null) return s
-      else  target["service"]=s
-      
-      
-  }
-}
+export {AuthService} from "./auth";
+export {AttachmentService} from "./attachments";
+export {UserService} from "./users";
+export {AuthorService} from "./authors";
+export {ArticleService} from "./articles";
+export {UserTicketService} from "./users.tickets";
+export {PayTabService} from "./paytabs";
