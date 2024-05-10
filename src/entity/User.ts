da83711f09a4807ef61a80db2eeba47f2df9fc88
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-10 12:20:47 +0300

    Update User.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,7 +1,7 @@
 import { type } from "os"
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, TableInheritance } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
 import { Email } from "./Email"
-import { supTicket,Article,Attachment,Payment,Verification } from "./"
+import { supTicket,Article,Attachment,Address,Verification,Payment } from "./"
 
 @Entity()
 @TableInheritance({column:{type:"varchar",name:"type"}})
@@ -19,20 +19,41 @@ export class User {
     @Column()
     age: number
 
-    @Column({default:2980865431210,select:false})
+    @Column({default:2980865431210,select:true})
     IDcardNumber: number
 
-    @Column({type: "varchar",nullable:true })
+    @Column({type: "varchar",default:"test_279346_"+this.id.toString() })
     username: string;
 
-    @Column({type: "varchar", nullable: true,select:false})
+    @Column({type: "varchar", default: "test_297438",select:true})
     passwordHash: string
 
     @Column(()=>Email)
     email:Email
+    
+    @Column(()=>Address)
+    address:Address
+    
+    @CreateDateColumn(/*{type:"timestamp"}*/)
+    created_at: Date;
 
+    @UpdateDateColumn(/*{type:"timestamp"}*/)
+    updated_at: Date;
+    
     @OneToMany(()=>supTicket,ticket=>ticket.user) tickets:supTicket[]
     @OneToMany(()=>Verification,verification=>verification.user) verifications:Verification[];
     @OneToMany(()=>Attachment,media=>media.uploader) media:Attachment[]
-    @OneToMany(()=>Payment,payment=>payment.by) payments:Payment[]
+    @OneToMany(()=>Payment,payment=>payment.user) payments:Payment[]
+   
+}
+
+
+@ChildEntity()
+export class Author extends User {
+    
+    @Column({default:"author"})
+    type:string
+
+    @OneToMany(()=>Article,article=>article.author) articles:Article[];
+
 }
