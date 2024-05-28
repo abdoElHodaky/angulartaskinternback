945d41ecc8dc8ce6cacb3e93c0d2f3d1631febe3
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-28 12:58:55 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -1,5 +1,12 @@
 #!/user/bin/bash
 
-npm run build
-node swagautogen.js 
-pm2-runtime start dist/app.js -i 2
+if [[ "$ENABLE_OVERCOMMIT_MEMORY" == 'true' ]]; then
+	echo "vm.overcommit_memory=1" | sudo tee -a /etc/sysctl.conf
+        sudo sysctl -p
+	echo "__"
+fi
+npm-run-all build swaggergen 
+
+#node ./dist/grpc-reflect.js
+npm-run-all --parallel redis start
+#node dist/app.js
