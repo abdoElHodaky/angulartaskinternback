2a0be0407fa1d05c10c9e119f94877533b7616a8
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-02 11:48:45 +0300

    Update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,3 +6,11 @@ export function nationalIdvalid(value:string){
     const pattern=/^([1-9]{1})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})[0-9]{3}([0-9]{1})[0-9]{1}$/
     return (pattern.test(message));
    }
+
+export function dateToReadable(date:Date|string){
+  var moment = require('moment');
+  require('moment/locale/ar');
+  let m=moment(date)
+  //console.log(moment.locale());
+  return m.format("dddd, MMMM Do YYYY, h:mm:ss a")
+}
