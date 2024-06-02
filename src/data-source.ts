88a3da8b77c0432bcacafd92904318b4402fc51a
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-02 18:31:29 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -19,9 +19,9 @@ export const _AppDataSource = new DataSource({
     synchronize: true,
     logging:"all",
     logger:"advanced-console",
-    entities: /*[User,Author,Attachment,Article,
-       Verification,supTicket,Payment,Book]*/
-    "./entity/index.ts",
+    entities: [User,Author,Attachment,Article,
+       Verification,supTicket,Payment,Book]
+    ,
     migrations: [],
     subscribers: [],
     extra: {
